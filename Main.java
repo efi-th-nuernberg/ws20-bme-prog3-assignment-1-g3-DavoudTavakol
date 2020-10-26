97c7482c1058cf54452da407ad2e1909c5cf5f89
repl.it user <replituser@example.com>
2020-10-26 14:26:09 +0000

    numbers

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello Davoud!");
+
+    int number = 200;
+    printNumberTo(number);
   }
+
+  public static void printNumberTo(int n) {
+    for (int i = 0; i <= n; i++) {
+      System.out.println(i);
+    }
+  }
+
 }
