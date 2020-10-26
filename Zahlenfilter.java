1cd8bb1fb69a3bc205157ca4835300fe80cea24b
repl.it user <replituser@example.com>
2020-10-26 16:21:25 +0000

    klasse erstellt

---
+++ Zahlenfilter.java
@@ -0,0 +1,23 @@
+class Zahlenfilter {
+
+  public static void main(String[] args) {
+    int number = 200;
+    printNumberTo(number);
+  }
+
+  public static void printNumberTo(int n) {
+    for (int i = 1; i <= n; i++) {
+      if (i % 5 == 0) {
+        System.out.println(i + " Zahl ist durch 5 Teilbar!");
+      }
+      if (i % 10 == 9) {
+        System.out.println(i + " Zahl endet mit einer 9!");
+      }
+      if ((i + i - 1) % 3 == 0) {
+        System.out.println(i + " Zahl addiert mit vorgänger ist durch 3 teilbar!");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}
